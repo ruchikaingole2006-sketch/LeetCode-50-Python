aa3bd7ff4afef71b246cc232f43511d85dadb449
ruchikaingole2006-sketch <ruchikaingole2006@gmail.com>
2026-09-19 14:17:19 +0530

    Create 45_Daily_Temperatures.py
    Add 45 Daily Temperatures solution

---
+++ 45_Daily_Temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                previous = stack.pop()
+                result[previous] = i - previous
+
+            stack.append(i)
+
+        return result
